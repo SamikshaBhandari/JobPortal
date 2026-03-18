17615e5ed280f84e5b147b2a298fb832468effed
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-03-18 17:24:36 +0545

    Enhance job controller with update and delete functionalities, including error handling and response messages.

--- jobportal_backend/controllers/jobController.js
+++ jobportal_backend/controllers/jobController.js
@@ -12,7 +12,7 @@ exports.postJob = async (req, res) => {
         });
     } catch (err) {
         console.error("Job Post Error:", err);
-        res.status(500).json({ message: "Job post garna milena, database check garney suru ma ." });
+        res.status(500).json({ message: "Job post garna milena database check garney suru ma ." });
     }
 };
 // @ts-ignore
@@ -23,5 +23,36 @@ exports.getAllJobs = async (req, res) => {
     } catch (err) {
         console.error("Fetch Jobs Error:", err);
         res.status(500).json({ message: "Jobs fetch garna sakina." });
+    }
+};
+exports.updateJob = async (req, res) => {
+    const { id } = req.params; 
+    const { title, description, company, location, salary } = req.body;
+
+    try {
+        const sql = "UPDATE jobs SET title=?, description=?, company=?, location=?, salary=? WHERE id=?";
+        const [result] = await db.query(sql, [title, description, company, location, salary, id]);
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: "Job bhetiyena" });
+        }
+        res.status(200).json({ message: "Job successfully update bhayo" });
+    } catch (err) {
+        res.status(500).json({ message: "Update garna milena." });
+    }
+};
+
+exports.deleteJob = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const [result] = await db.query("DELETE FROM jobs WHERE id = ?", [id]);
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: "Job bhetiyena." });
+        }
+        res.status(200).json({ message: "Job successfully delete bhayo." });
+    } catch (err) {
+        res.status(500).json({ message: "Delete garna milena." });
     }
 };
