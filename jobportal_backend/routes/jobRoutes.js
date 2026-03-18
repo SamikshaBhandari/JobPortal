cc1057dd77ae9bbda514f925df2d6b705a5272e1
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-03-18 17:24:51 +0545

    Add update and delete job routes to job controller

--- jobportal_backend/routes/jobRoutes.js
+++ jobportal_backend/routes/jobRoutes.js
@@ -4,5 +4,6 @@ const jobController = require('../controllers/jobController');
 
 router.post('/jobpost', jobController.postJob);
 router.get('/alljobs', jobController.getAllJobs);
-
+router.put('/updatejob/:id', jobController.updateJob);
+router.delete('/deletejob/:id', jobController.deleteJob);
 module.exports = router;
